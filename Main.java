f75fe490e9879b77bdcdb9fd6eb19210f3e654b2
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-16 23:33:41 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,9 @@ public class RecursionRunner
 	public static void main(String args[])
 	{
 		System.out.println(RecursionFunTwo.countChickens("itatfun"));
-		
-		//add more test cases
+		System.out.println(RecursionFunTwo.countChickens("itatchickenfun"));
+		System.out.println(RecursionFunTwo.countChickens("chchickchickenenicken"));
+		System.out.println(RecursionFunTwo.countChickens("chickchickfun"));
+		System.out.println(RecursionFunTwo.countChickens("chickenbouncetheballchicken"));
 	}
 }
